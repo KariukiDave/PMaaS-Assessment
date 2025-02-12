2f2f498528861f568c30b889332bb1fed6ff0ac7
David Kariuki <103850268+KariukiDave@users.noreply.github.com>
2025-02-13 01:23:42 +0300

    PM Assessment Tool v1.2.1 Updated dashboard charts

--- pm-assessment-tool.php
+++ pm-assessment-tool.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Project Management Assessment Tool
+ * Plugin Name: PM Assessment Tool
  * Description: A tool to assess project management needs and provide recommendations
  * Version: 1.2.1
  * Author: David Kariuki
